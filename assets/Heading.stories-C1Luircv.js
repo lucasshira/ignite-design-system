b92de198fc9bc4589fd068e3a0efd0cad206a5b0
lucasshira <lucasshira@users.noreply.github.com>
2025-05-16 16:12:29 +0000

    deploy: f76a7621903d95b08675d2da2f6a0910a9c93525

---
+++ assets/Heading.stories-C1Luircv.js
@@ -0,0 +1,13 @@
+import{H as c}from"./index-DKRt0IaG.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";const h={title:"Typography/Heading",component:c,tags:["autodocs"],args:{children:"Custom Heading - $default"}},a={},e={args:{children:"H1 Heading",as:"h1"},parameters:{docs:{description:{story:"By default, the Heading will always be an `h2`, but we can change it using the `as` prop."}}}};var r,s,t;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(s=a.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var o,n,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'H1 Heading',
+    as: 'h1'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'By default, the Heading will always be an \`h2\`, but we can change it using the \`as\` prop.'
+      }
+    }
+  }
+}`,...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const H=["Primary","CustomTag"];export{e as CustomTag,a as Primary,H as __namedExportsOrder,h as default};
