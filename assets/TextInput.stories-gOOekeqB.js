00134f24902f2435abd85f1dd791cb51916914ab
lucasshira <lucasshira@users.noreply.github.com>
2025-05-16 12:17:11 +0000

    deploy: 5ce2fe6f974d418a760a763c6a0e307e0963845a

---
+++ assets/TextInput.stories-gOOekeqB.js
@@ -0,0 +1,14 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{b as x,B as g,T as f}from"./index-CPTHmIaS.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";const D={title:"Form/TextInput",component:x,tags:["autodocs"],decorators:[u=>a.jsxs(g,{as:"label",css:{display:"flex",flexDirection:"column",gap:"$2"},children:[a.jsx(f,{size:"sm",children:"E-mail address"}),u()]})]},r={args:{placeholder:"Type your name"}},e={args:{prefix:"cal.com/",placeholder:"your-username"}},s={args:{disabled:!0}};var o,t,c;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Type your name'
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var n,m,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    prefix: 'cal.com/',
+    placeholder: 'your-username'
+  }
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var i,d,l;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(l=(d=s.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const E=["Primary","WithPrefix","Disabled"];export{s as Disabled,r as Primary,e as WithPrefix,E as __namedExportsOrder,D as default};
