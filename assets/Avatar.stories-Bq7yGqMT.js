956522d849609be5719540e9a4931c0a16282fc9
lucasshira <lucasshira@users.noreply.github.com>
2025-05-19 23:00:42 +0000

    deploy: 12ec16db151068c011c057e56a8250df0630067b

--- assets/Avatar.stories-Bq7yGqMT.js
+++
@@ -1,5 +0,0 @@
-import{A as p}from"./index-o_1rkHwq.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";const h={title:"Data Display/Avatar",component:p,tags:["autodocs"],args:{src:"https://github.com/lucasshira.png",alt:"Lucas Shira"},argTypes:{src:{control:{type:"text"}}}},r={},a={args:{src:void 0}};var s,t,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var e,c,i;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    src: undefined
-  }
-}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const y=["Primary","WithFallback"];export{r as Primary,a as WithFallback,y as __namedExportsOrder,h as default};
